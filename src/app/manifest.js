2f36431deebd621fe2e2e1e9492ed6f716fdeff7
ayush <ayushatschool5@gmail.com>
2025-09-02 22:55:17 +0530

    intial commit

---
+++ src/app/manifest.js
@@ -0,0 +1,11 @@
+export default function manifest() {
+    return {
+        name: "Next.js PWA",
+        short_name: "NextPWA",
+        description: "A Progressive Web App built with Next.js",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+    };
+}
